6ec229fbe43aca615cc0bb5c15e72c4b4d7eb8ad
mariaonderoz <mskorneeva.87@gmail.com>
2024-07-31 23:42:52 +0700

    corrected

--- habits/tests.py
+++ habits/tests.py
@@ -16,13 +16,13 @@ class HabitTestCase(APITestCase):
         self.habit = Habit.objects.create(
             owner=self.user,
             place='Дом',
-            start_time='07:30',
+            start_time='07:30:00',
             action='Сделать зарядку',
             is_pleasant=True,
             related_habit=None,
             periodicity=1,
             reward='Чашка кофе',
-            execution_time='00:15'
+            execution_time='00:15:00'
         )
 
     def test_habit_create(self):
@@ -32,32 +32,29 @@ class HabitTestCase(APITestCase):
         data = {
             'owner': self.user.id,
             'place': 'Дом',
-            'start_time': '07:30',
+            'start_time': '07:30:00',
             'action': 'Сделать зарядку',
             'is_pleasant': True,
             'periodicity': 1,
-            'reward': 'Чашка кофе',
-            'execution_time': '00:15'
+            'execution_time': '00:15:00'
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Habit.objects.all().count(), 1)
+        self.assertEqual(Habit.objects.all().count(), 2)
 
         url = reverse('habits:habit_create')
         data = {
             'owner': self.user.id,
             'place': 'Квартира',
-            'start_time': '17:30',
+            'start_time': '17:30:00',
             'action': 'Принять участие в онлайн-игре',
             'is_pleasant': False,
-            'related_habit': self.habit.id,
             'periodicity': 3,
-            'reward': 'Подарок на день рождения',
-            'execution_time': '00:30'
+            'execution_time': '00:30:00'
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Habit.objects.all().count(), 2)
+        self.assertEqual(Habit.objects.all().count(), 3)
 
     def test_habit_list(self):
         """Тестирование получения списка всех привычек"""
@@ -78,7 +75,7 @@ class HabitTestCase(APITestCase):
         self.assertEqual(data.get("start_time"), self.habit.start_time)
         self.assertEqual(data.get("action"), self.habit.action)
         self.assertEqual(data.get("is_pleasant"), self.habit.is_pleasant)
-        self.assertEqual(data.get("related_habit"), self.habit.related_habit.id)
+        self.assertEqual(data.get("related_habit"), self.habit.related_habit)
         self.assertEqual(data.get("periodicity"), self.habit.periodicity)
         self.assertEqual(data.get("reward"), self.habit.reward)
         self.assertEqual(data.get("execution_time"), self.habit.execution_time)
@@ -92,9 +89,7 @@ class HabitTestCase(APITestCase):
             'start_time': '17:30',
             'action': 'Принять участие в онлайн-игре',
             'is_pleasant': False,
-            'related_habit': self.habit.id,
             'periodicity': 3,
-            'reward': 'Подарок на день рождения',
             'execution_time': '00:30'
         }
         response = self.client.patch(url, data, format='json')
@@ -103,15 +98,13 @@ class HabitTestCase(APITestCase):
         self.assertEqual(data.get("start_time"), "17:30")
         self.assertEqual(data.get("action"), "Принять участие в онлайн-игре")
         self.assertEqual(data.get("is_pleasant"), False)
-        self.assertEqual(data.get("related_habit"), self.habit.id)
         self.assertEqual(data.get("periodicity"), 3)
-        self.assertEqual(data.get("reward"), "Подарок на день рождения")
         self.assertEqual(data.get("execution_time"), "00:30")
 
     def test_habit_destroy(self):
         """Тестирование удаления привычки"""
 
-        url = reverse('habits:habit_destroy', args=(self.habit.pk,))
+        url = reverse('habits:habit_delete', args=(self.habit.pk,))
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Habit.objects.all().count(), 0)
